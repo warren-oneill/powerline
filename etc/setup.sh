4d587dc4e7acf36fe45b39a782efc9fc1f9d15ff
warren-oneill <warren.oneill@grundgruen.com>
2015-03-27 17:16:46 +0100

    added sql source and read data in test algo

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r ../zipline/etc/requirements.txt
-pip3 install -r ../zipline/etc/requirements_dev.txt
-python3 ../zipline/setup.py install
+cd ../zipline
+python3 setup.py install
