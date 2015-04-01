1242e8b625b07283e3be418647fc3c4a680c6ccb
warren-oneill <warren.oneill@grundgruen.com>
2015-04-01 17:55:35 +0200

    added calendar unittest and fix pep8 issues

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-pip3 install -r ../zipline/etc/requirements.txt
-cd ../zipline
-python3 setup.py install
+pip3 install --allow-all-external -r etc/requirements.txt
+pip3 install -e git://github.com/warren-oneill/zipline.git@cal-clean#egg=zipline-futures
