0775ceeeeb00142397d1d94f6521f955f8d9e611
warren-oneill <warren.oneill@grundgruen.com>
2015-04-07 13:45:49 +0200

    update algo, tests, TradingEnv...

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install --allow-all-external -r etc/requirements.txt
-pip3 install -e git://github.com/warren-oneill/zipline.git@cal-clean#egg=zipline-futures
+pip3 install -e git://github.com/grundgruen/zipline.git@gg-futures#egg=zipline-futures
---
+++ powerline/data/loader_power.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from collections import OrderedDict
+from six import iteritems
+from math import pow
+
+from zipline.data.loader import dump_treasury_curves, get_data_filepath
+
+from powerline.utils.tradingcalendar_eex import trading_day as trading_day_eex
+from powerline.utils.tradingcalendar_eex import trading_days as trading_days_eex
+
+INDEX_MAPPING = {
+    '^GSPC':
+    ('treasuries', 'treasury_curves.csv', 'data.treasury.gov'),
+    '^GSPTSE':
+    ('treasuries_can', 'treasury_curves_can.csv', 'bankofcanada.ca'),
+    '^FTSE':  # use US treasuries until UK bonds implemented
+    ('treasuries', 'treasury_curves.csv', 'data.treasury.gov'),
+}
+
+
+def load_market_data(trading_day=trading_day_eex,
+                     trading_days=trading_days_eex, bm_symbol='^EEX'):
+    # generate constant daily returns for an annualised rate of 20%
+    daily_return = pow(1.2, 1.0/365.0)-1.0
+    benchmark_returns = pd.Series(daily_return, index=trading_days)
+
+    most_recent = pd.Timestamp('today', tz='UTC') - trading_day
+    most_recent_index = trading_days.searchsorted(most_recent)
+    days_up_to_now = trading_days[:most_recent_index + 1]
+
+    # Get treasury curve module, filename & source from mapping.
+    # Default to USA.
+    module, filename, source = INDEX_MAPPING.get(
+        bm_symbol, INDEX_MAPPING['^GSPC'])
+
+    tr_filepath = get_data_filepath(filename)
+    try:
+        saved_curves = pd.DataFrame.from_csv(tr_filepath)
+    except (OSError, IOError):
+        print("""
+data files aren't distributed with source.
+Fetching data from {0}
+""".format(source).strip())
+        dump_treasury_curves(module, filename)
+        saved_curves = pd.DataFrame.from_csv(tr_filepath)
+
+    # Find the offset of the last date for which we have trading data in our
+    # list of valid trading days
+    last_tr_date = saved_curves.index[-1]
+    last_tr_date_offset = days_up_to_now.searchsorted(
+        last_tr_date.strftime('%Y/%m/%d'))
+
+    # If more than 1 trading days has elapsed since the last day where
+    # we have data,then we need to update
+    # Comment above explains why this is "> 2".
+    if len(days_up_to_now) - last_tr_date_offset > 2:
+        treasury_curves = dump_treasury_curves(module, filename)
+    else:
+        treasury_curves = saved_curves.tz_localize('UTC')
+
+    tr_curves = {}
+    for tr_dt, curve in treasury_curves.T.iteritems():
+        # tr_dt = tr_dt.replace(hour=0, minute=0, second=0, microsecond=0,
+        #                       tzinfo=pytz.utc)
+        tr_curves[tr_dt] = curve.to_dict()
+
+    tr_curves = OrderedDict(sorted(
+        ((dt, c) for dt, c in iteritems(tr_curves)),
+        key=lambda t: t[0]))
+
+    return benchmark_returns, tr_curves
