e4a7c2bbb8112871e163c0a8209cf39d1aa58f82
warren-oneill <warren.oneill@grundgruen.com>
2015-04-09 14:50:12 +0200

    algo updates, new zipline branch

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install --allow-all-external -r etc/requirements.txt
-pip3 install -e git://github.com/grundgruen/zipline.git@gg-futures#egg=zipline-futures
+pip3 install -e git://github.com/quantopian/zipline.git@gg-futures#egg=futures-trading
