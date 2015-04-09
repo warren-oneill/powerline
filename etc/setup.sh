91c17f836a2f7724f0bf293c4cb2d06db99e5ffb
Stefan Hackmann <stefan.hackmann@grundgruen.com>
2015-04-09 15:27:51 +0200

    plt.show()

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install --allow-all-external -r etc/requirements.txt
-pip3 install -e git://github.com/quantopian/zipline.git@gg-futures#egg=futures-trading
+pip3 install -e git://github.com/quantopian/zipline.git@futures-trading#egg=futures-trading
