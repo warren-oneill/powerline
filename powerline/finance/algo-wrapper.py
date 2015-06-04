992490c8e05abc460cb63a29622af33763ca56d0
Stefan Hackmann <stefan.hackmann@grundgruen.com>
2015-06-04 13:59:53 +0200

    please add doc string for each class and method

--- powerline/finance/algo-wrapper.py
+++ powerline/finance/algo-wrapper.py
@@ -1,8 +1,10 @@
 from zipline.algorithm import TradingAlgorithm
 
-
+#   TODO pls add doc strings for each class and for each method
 class AlgoWrapper():
+
     def __init__(self, exchange, optimize, initialize, handle_data):
+        # TODO type """ and hit return to enter standard documentation for methods
         self.exchange = exchange
         self.optimize = optimize
         self.algo = TradingAlgorithm(initialize=initialize,
