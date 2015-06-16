6fb4c9e964a37a025497274b6ce8c7c39c8bb6e7
warren-oneill <warren.oneill@grundgruen.com>
2015-06-16 14:18:17 +0200

    ENH, TST: first banal attempt at auction order function

---
+++ gg/powerline/test_algorithms.py
@@ -0,0 +1,40 @@
+from gg.powerline.finance.auction import TradingAlgorithmGG
+
+
+class TestAuctionAlgorithm(TradingAlgorithmGG):
+    """
+    This algorithm will send a specified number of orders, to allow unit tests
+    to verify the orders sent/received, transactions created, and positions
+    at the close of a simulation.
+    """
+
+    def initialize(self,
+                   sid,
+                   amount,
+                   order_count,
+                   day,
+                   sid_filter=None,
+                   slippage=None,
+                   commission=None):
+        self.count = order_count
+        self.asset = self.sid(sid)
+        self.amount = amount
+        self.day = day
+        self.incr = 0
+
+        if sid_filter:
+            self.sid_filter = sid_filter
+        else:
+            self.sid_filter = [self.asset.sid]
+
+        if slippage is not None:
+            self.set_slippage(slippage)
+
+        if commission is not None:
+            self.set_commission(commission)
+
+    def handle_data(self, data):
+        # place an order for amount shares of sid
+        if self.incr < self.count:
+            self.order_auction(day=self.day, amounts=self.amount)
+            self.incr += 1
