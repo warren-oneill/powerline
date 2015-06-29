89da0b0ba14d786680d5bd705adbf64bdc71e4a7
warren-oneill <warren.oneill@grundgruen.com>
2015-06-29 13:16:37 +0200

    BLD: freezes zipline version

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install --allow-all-external -r etc/requirements.txt
-pip3 install -e git://github.com/quantopian/zipline.git#egg=zipline
+pip3 install -e git://github.com/quantopian/zipline.git@cc77a523220c980f3c741b073a0f3eea5ca8db51#egg=zipline
