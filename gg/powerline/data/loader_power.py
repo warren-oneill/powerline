91628e24845663f93ee3891ab8475486fdaf2225
warren-oneill <warrenon@gmail.com>
2015-10-28 12:00:48 +0100

    MAINTS: updates zipline env

--- gg/powerline/data/loader_power.py
+++ gg/powerline/data/loader_power.py
@@ -4,7 +4,7 @@ import pandas as pd
 from math import pow
 import numpy as np
 
-from zipline.data.loader import dump_treasury_curves, get_data_filepath
+from zipline.data.loader import ensure_treasury_data
 
 INDEX_MAPPING = {
     '^GSPC':
@@ -35,39 +35,15 @@ def load_market_data(trading_day,
     for dt, value in benchmark_returns.iteritems():
         benchmark_returns[dt] = value + np.random.randn() * sd
 
-    most_recent = pd.Timestamp('today', tz='UTC') - trading_day
-    most_recent_index = trading_days.searchsorted(most_recent)
-    days_up_to_now = trading_days[:most_recent_index + 1]
-
-    # Get treasury curve module, filename & source from mapping.
-    # Default to USA.
-    module, filename, source = INDEX_MAPPING.get(
-        bm_symbol, INDEX_MAPPING['^GSPC'])
-
-    tr_filepath = get_data_filepath(filename)
-    try:
-        saved_curves = pd.DataFrame.from_csv(tr_filepath)
-    except (OSError, IOError):
-        print("""
-data files aren't distributed with source.
-Fetching data from {0}
-""".format(source).strip())
-        dump_treasury_curves(module, filename)
-        saved_curves = pd.DataFrame.from_csv(tr_filepath)
-
-    # Find the offset of the last date for which we have trading data in our
-    # list of valid trading days
-    last_tr_date = saved_curves.index[-1]
-    last_tr_date_offset = days_up_to_now.searchsorted(
-        last_tr_date.strftime('%Y/%m/%d'))
-
-    # If more than 1 trading days has elapsed since the last day where
-    # we have data,then we need to update
-    # Comment above explains why this is "> 2".
-    if len(days_up_to_now) - last_tr_date_offset > 2:
-        treasury_curves = dump_treasury_curves(module, filename)
-    else:
-        treasury_curves = saved_curves.tz_localize('UTC')
-
+    first_date = trading_days[0]
+    last_date = trading_days[
+        trading_days.get_loc(pd.Timestamp.utcnow(), method='ffill') - 2
+    ]
+    treasury_curves = ensure_treasury_data(
+        bm_symbol,
+        first_date,
+        last_date,
+    )
     treasury_curves = treasury_curves.reindex(trading_days, method='ffill')
+
     return benchmark_returns, treasury_curves
