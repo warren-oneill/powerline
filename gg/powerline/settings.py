7f8f8d91338682adc6c6c74233d8d9b0b517ea59
Stefan Hackmann <dhexus314@gmail.com>
2015-11-12 07:29:18 +0100

    test db and volume workaround

--- gg/powerline/settings.py
+++ gg/powerline/settings.py
@@ -1,5 +1,5 @@
 __author__ = 'Warren'
 
-from gg.database.mysql_conf import mysql_connection_aws as mysql_connection
+from gg.database.mysql_conf import mysql_connection as mysql_connection
 
 connection = mysql_connection
