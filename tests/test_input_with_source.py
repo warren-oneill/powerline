29aba0a35caf5b4328e2b3f84d611aa1c0e4f160
dev <stefan.hackmann@grundgruen.com>
2015-11-30 13:07:09 +0100

    wrap existing functionality without new features

--- tests/test_input_with_source.py
+++ tests/test_input_with_source.py
@@ -1,41 +1,42 @@
-from datetime import datetime
+from datetime import datetime, date
 import pandas as pd
 from unittest import TestCase
 from gg.powerline.exchanges.eex_exchange import EexExchange
+
+
+__author__ = "Stefan Hackmann"
 
 
 class TestEexSourceWithInput(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
-        # cls.start = pd.Timestamp(datetime(day=20, month=6, year=2015),
-        #                          tz='Europe/Berlin').tz_convert('UTC')
-        # cls.end = pd.Timestamp(datetime(day=25, month=6, year=2015),
-        #                        tz='Europe/Berlin').tz_convert('UTC')
-        # cls.exchange = EexExchange(start=cls.start, end=cls.end, products=[
-        #     'F1B1', 'F1B2'])
-        # cls.exchange.env.write_data(
-        #     futures_data=cls.exchange.asset_metadata)
+        cls.start = pd.Timestamp(datetime(day=20, month=6, year=2015),
+                                 tz='Europe/Berlin').tz_convert('UTC')
+        cls.end = pd.Timestamp(datetime(day=25, month=6, year=2015),
+                               tz='Europe/Berlin').tz_convert('UTC')
+        cls.exchange = EexExchange(start=cls.start, end=cls.end,
+                                   type="input", products=['F1B1', 'F1B2'])
+        cls.exchange.env.write_data(
+            futures_data=cls.exchange.asset_metadata)
 
     def test_input_source(self):
-        pass
-        # source = self.exchange.source
-        # self.assertIsInstance(source.start, pd.lib.Timestamp)
-        # self.assertIsInstance(source.end, pd.lib.Timestamp)
-        # last_dt = self.start
-        # for event in source:
-        #     self.assertIsInstance(event.dt, date)
-        #     self.assertGreaterEqual(event.dt, last_dt)
-        #     self.assertIsInstance(event.volume, int)
-        #     self.assertGreater(event.volume, 0)
-        #     self.assertIsInstance(event.sid, int)
-        #     self.assertIsInstance(event.price, float)
-        #     self.assertIn(event.product, ['F1B1', 'F1B2'])
-        #
-        #     self.assertTrue(abs(event.price) >= 0.01 or abs(event.price) == 0)
-        #     self.assertGreaterEqual(event.dt, self.start)
-        #     self.assertLessEqual(event.dt, self.end)
+        source = self.exchange.source
+        self.assertIsInstance(source.start, pd.lib.Timestamp)
+        self.assertIsInstance(source.end, pd.lib.Timestamp)
+        last_dt = self.start
+        for event in source:
+            self.assertIsInstance(event.dt, date)
+            self.assertGreaterEqual(event.dt, last_dt)
+            self.assertIsInstance(event.volume, int)
+            self.assertGreater(event.volume, 0)
+            self.assertIsInstance(event.sid, int)
+            self.assertIsInstance(event.price, float)
+            self.assertIn(event.product, ['F1B1', 'F1B2'])
+
+            self.assertTrue(abs(event.price) >= 0.01 or abs(event.price) == 0)
+            self.assertGreaterEqual(event.dt, self.start)
+            self.assertLessEqual(event.dt, self.end)
 
     @classmethod
     def tearDownClass(cls):
