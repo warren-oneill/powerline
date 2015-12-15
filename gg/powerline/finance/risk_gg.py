MAINT: Corrected VaR calculation and improved unittests

gg/powerline/finance/risk_gg.py
@@ -26,7 +26,7 @@ class RiskReport(object):
         self.max_drawdown_duration = pd.Timestamp(0)
 
         self.benchmark = perf.benchmark_period_return
-        self.benchmark_profit = perf.benchmark_period_return[-1]
+        self.benchmark_returns = perf.benchmark_period_return[-1]
 
         start = perf.index[0].strftime('%Y-%m-%d')
         end = perf.index[-1].strftime('%Y-%m-%d')
@@ -62,6 +62,7 @@ class RiskReport(object):
         Variance-Covariance calculation of daily Value-at-Risk based on a
         normal distribution model with mean of returns mu and standard
         deviation of returns sigma, on a portfolio of value P.
+        see page 9: https://people.math.ethz.ch/~embrecht/ftp/LongTermRisk.pdf
 
         :param c: confidence level of the calculation
         :param n: length of the considered time period (multiple of the base
@@ -72,10 +73,9 @@ class RiskReport(object):
         sigma = self.returns.std()
         P = self.profit
 
-        alpha = norm.ppf(1 - c, mu, sigma)
-        sqrt_n = np.sqrt(n)
+        alpha = norm.ppf(1 - c, n*mu, np.sqrt(n)*sigma)
 
-        return - sqrt_n * P * alpha - (n - sqrt_n) * mu
+        return - P * alpha
 
     def calculate_win_loss(self):
         """
@@ -107,7 +107,8 @@ class RiskReport(object):
                 ["VaR 99 (€)", self.var_99],
                 ["VaR 95 (€), 5 days", self.five_day_var_95],
                 ["VaR 99 (€), 5 days", self.five_day_var_99],
-                ["Win Loss Ratio", self.win_loss]]
+                ["Win Loss Ratio", self.win_loss],
+                ["Total Benchmark return", self.benchmark_returns]]
         headers = ["Risk Report", self.period]
 
         pd.concat([self.returns.cumsum(), self.benchmark], axis=1).plot()

tests/test_risk.py
@@ -27,9 +27,21 @@ class TestRiskReport(TestCase):
         """
         expected_var_95 = 6.87574503
         expected_var_99 = 10.6897709
+        expected_var_95_5_day = 8.9352718397
+        expected_var_99_5_day = 17.4636929683
+        expected_var_95_10_day = 5.8126066176
+        expected_var_99_10_day = 17.8736154433
 
         self.assertAlmostEqual(self.metrics.var_95, expected_var_95)
         self.assertAlmostEqual(self.metrics.var_99, expected_var_99)
+        self.assertAlmostEqual(self.metrics.five_day_var_95,
+                               expected_var_95_5_day)
+        self.assertAlmostEqual(self.metrics.five_day_var_99,
+                               expected_var_99_5_day)
+        self.assertAlmostEqual(self.metrics.calculate_var(0.95, 10),
+                               expected_var_95_10_day)
+        self.assertAlmostEqual(self.metrics.calculate_var(0.99, 10),
+                               expected_var_99_10_day)
 
     def test_win_loss(self):
         expected_win_loss = 1.33
@@ -61,12 +73,12 @@ class TestRiskReport(TestCase):
         # portfolio value = 200, 100, 180, 210.6, 421.2, 379.8, 208.494
         returns = pd.Series([1.0, -0.5, 0.8, .17, 1.0, -0.1, -0.45])
         pnl = pd.Series([0, -100, 80, 20.6, 220.6, -41.4, -171.306])
+        benchmark = pd.Series([200, 204, 205, 203, 204, 206, 208])
+        max_drawdown = pd.Series([0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.495])
 
         # the following do not need to be tested as they are provided
         # by zipline
         returns_period = pd.Series(np.zeros(7))
-        max_drawdown = pd.Series(np.zeros(7))
-        benchmark = pd.Series(np.zeros(7))
         sortino = pd.Series(np.zeros(7))
         sharpe = pd.Series(np.zeros(7))
         information = pd.Series(np.zeros(7))