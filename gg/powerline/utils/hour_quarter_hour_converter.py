c8fb6359efedbcd3ad627763ea4e75f7b296e034
Maximilian König <maximilian.koe@gmail.com>
2015-12-17 16:29:53 +0100

    MAINT: combined the two functions into one

--- gg/powerline/utils/hour_quarter_hour_converter.py
+++ gg/powerline/utils/hour_quarter_hour_converter.py
@@ -1,8 +1,8 @@
-__author__ = 'max'
-
 import pandas as pd
 import numpy as np
 from itertools import product
+
+__author__ = 'max'
 
 hourly_products = ['%02d-%02d' % (i, i+1) for i in range(24)]
 quarter_tags = ['Q%d' % i for i in range(1, 5)]
@@ -10,29 +10,24 @@ quarterly_products = ['%02d%s' % (i, tag)
                       for (i, tag) in product(range(24), quarter_tags)]
 
 
-def convert_h_to_qh(h_data):
+def convert_between_h_and_qh(source_frame):
     """
-    Convert a DataFrame containing hourly price data (e.g. the history of
-    'epex_auction') into one with quarter hour prices by copying one hour's
-    prices into it's four associated quarters
-    :param h_data: DataFrame with the hourly price data
-    :return: DataFrame with quarter hour prices
+    Convert a DataFrame with hourly or quarter hour price data to the other
+    format
+    :param source_frame: DataFrame with hourly or quarter hour prices
+    :return: DataFrame with quarter hour or hourly prices
     """
-    qh_data = pd.DataFrame(np.array(h_data).repeat(4, axis=1),
-                           index=h_data.index, columns=quarterly_products)
-    return qh_data
-
-
-def convert_qh_to_h(qh_data):
-    """
-    Convert a DataFrame containing quarter hour prices (e.g. the history of
-    'aepp') into one with hourly prices by averaging the prices
-    :param qh_data: DataFrame with the quarter hour price
-    :return: DataFrame with hourly prices
-    """
-    data = np.array(qh_data)
-    mean_data = np.mean(np.array([data[:, ::4], data[:, 1::4],
-                                  data[:, 2::4], data[:, 3::4]]), axis=0)
-    h_data = pd.DataFrame(mean_data,
-                          index=qh_data.index, columns=hourly_products)
-    return h_data
+    if source_frame.columns.shape[0] == 24:
+        result_frame = pd.DataFrame(np.array(source_frame).repeat(4, axis=1),
+                                    index=source_frame.index,
+                                    columns=quarterly_products)
+    elif source_frame.columns.shape[0] == 96:
+        data = np.array(source_frame)
+        mean_data = np.mean(np.array([data[:, ::4], data[:, 1::4],
+                            data[:, 2::4], data[:, 3::4]]), axis=0)
+        result_frame = pd.DataFrame(mean_data, index=source_frame.index,
+                                    columns=hourly_products)
+    else:
+        raise ValueError('Argument source_frame should be a Dataframe with ' +
+                         'either 24 or 96 columns')
+    return result_frame
